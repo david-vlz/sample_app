3462f78c768a677d7eca7691a48d89c404593820
= <Neugebauer_David@web.de>
2012-10-15 20:34:01 +0200

    use postgresql instead of sqlite, hopefully...

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,4 +37,15 @@ describe "Static pages" do
 		end
 	end
 	
+	describe "Contect page" do
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', :text => "#{base_title} | Contact")
+		end
+	end
+	
 end
