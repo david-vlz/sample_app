d22983581326aa125feec18511b0bb3e66035bbd
= <Neugebauer_David@web.de>
2012-10-27 01:48:39 +0200

    complete bootstrap and insert routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,49 +6,49 @@ describe "Static pages" do
 	
 	describe "Home page" do 
 		it "should have the h1 'Sample App'" do
-			visit '/static_pages/home' 
+			visit root_path 
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 		it "should have the basic title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title', :text => "#{base_title}")
 		end
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should_not have_selector('title', :text => " | Home")
 		end
 	end
 	
 	describe "Help page" do
 		it "shoud have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 		it "should have the right title" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
 	
 	describe "About page" do
 		it "should have the content 'About us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', :text => 'About Us')
 		end
 		it "should have the right title" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('title', :text => "#{base_title} | About")
 		end
 	end
 	
 	describe "Contect page" do
 		it "should have the h1 'Contact'" do
-			visit '/static_pages/contact'
-			page.should have_selector('h1', :text => 'Contact')
+			visit contact_path
+			page.should have_selector('h1', text: 'Contact')
 		end
 		it "should have the right title" do
-			visit '/static_pages/contact'
-			page.should have_selector('title', :text => "#{base_title} | Contact")
+			visit contact_path
+			page.should have_selector('title', text: "#{base_title} | Contact")
 		end
 	end
 	
