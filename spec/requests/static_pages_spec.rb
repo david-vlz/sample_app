4a8e1a6136ed8757d45d2e8f24037d484f68f283
= <Neugebauer_David@web.de>
2012-10-27 02:14:07 +0200

    add before blocks

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,49 +5,48 @@ describe "Static pages" do
 	let (:base_title) { "Ruby on Rails Tutorial Sample App" }
 	
 	describe "Home page" do 
+		before { visit root_path }
+
 		it "should have the h1 'Sample App'" do
-			visit root_path 
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 		it "should have the basic title" do
-			visit root_path
 			page.should have_selector('title', :text => "#{base_title}")
 		end
 		it "should not have a custom page title" do
-			visit root_path
 			page.should_not have_selector('title', :text => " | Home")
 		end
 	end
 	
 	describe "Help page" do
+		before { visit help_path }
+		
 		it "shoud have the content 'Help'" do
-			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 		it "should have the right title" do
-			visit help_path
 			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
 	
 	describe "About page" do
+		before { visit about_path }
+		
 		it "should have the content 'About us'" do
-			visit about_path
 			page.should have_selector('h1', :text => 'About Us')
 		end
 		it "should have the right title" do
-			visit about_path
 			page.should have_selector('title', :text => "#{base_title} | About")
 		end
 	end
 	
 	describe "Contect page" do
+		before { visit contact_path }
+		
 		it "should have the h1 'Contact'" do
-			visit contact_path
 			page.should have_selector('h1', text: 'Contact')
 		end
 		it "should have the right title" do
-			visit contact_path
 			page.should have_selector('title', text: "#{base_title} | Contact")
 		end
 	end
