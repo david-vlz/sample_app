4d8ae5c45c9d27ce094591b0218287efaca5d467
= <Neugebauer_David@web.de>
2012-11-03 20:43:34 +0100

    refactor test using utility function

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,20 +14,20 @@ describe "Static pages" do
 	
 	describe "Help page" do
 		before { visit help_path }
-		it { should have_selector('h1', :text => 'Help') }
-		it { should have_selector('title', :text => "#{base_title} | Help") }
+		it { should have_selector('h1', text: 'Help') }
+		it { should have_selector('title', text: full_title('Help')) }
 	end
 	
 	describe "About page" do
 		before { visit about_path }
-		it { should have_selector('h1', :text => 'About Us') }
-		it { should have_selector('title', :text => "#{base_title} | About") }
+		it { should have_selector('h1', text: 'About Us') }
+		it { should have_selector('title', text: full_title('About')) }
 	end
 	
 	describe "Contect page" do
 		before { visit contact_path }
 		it { should have_selector('h1', text: 'Contact') }
-		it { should have_selector('title', text: "#{base_title} | Contact") }
+		it { should have_selector('title', text: full_title('Contact')) }
 	end
 	
 end
