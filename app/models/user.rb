1eded10c0f860562283333e249931e0b6503cbf6
= <Neugebauer_David@web.de>
2012-11-04 09:48:26 +0100

    add password_digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,29 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
   
 	attr_accessible(:email, :name)
   
+  	
+  
+  	before_save { |user| user.email = email.downcase }
+  
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates(:name, presence: true, length: { maximum: 50, minimum: 3 });
-	validates(:email, presence: true, format: VALID_EMAIL_REGEX);
+	validates(:name, presence: true, 
+					 length: { maximum: 50, minimum: 3 }
+			 )
+	validates(:email, presence: true, 
+					  format: VALID_EMAIL_REGEX,
+					  uniqueness: { case_sensitive: false }
+			 )
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
-
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	before { @user = User.new(name: "Example User", email: "user@example.com") }
 	subject { @user }
@@ -22,6 +21,7 @@ describe User do
 	it { should respond_to(:name) }
 	it { should respond_to(:created_at) }
 	it { should respond_to(:updated_at) }
+	it { should respond_to(:password_digest) }
 	
 	it { should be_valid }
 	
@@ -42,6 +42,7 @@ describe User do
 		before { @user.email =" " }
 		it { should_not be_valid }
 	end
+	
 	describe "when email format is invalid" do
 		it "should be invalid" do
 			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
@@ -51,6 +52,7 @@ describe User do
 			end
 		end
 	end
+	
 	describe "when email format is valid" do
 		it "should be valid" do
 			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
@@ -59,6 +61,15 @@ describe User do
 				@user.should be_valid
 			end
 		end
+	end
+	
+	describe "when email is already taken" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
+		end
+		it { should_not be_valid }
 	end
 	
 end
