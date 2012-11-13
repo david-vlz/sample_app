3ab5a7d3ca8d3e1b78c28a7aa56328446846f798
= <Neugebauer_David@web.de>
2012-11-13 22:14:18 +0100

    add gravatar functionality

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+	
+	# Gravatar für einen User zurückgeben (http://gravatar.com/ )
+	def gravatar_for (user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+	end
+	
 end
